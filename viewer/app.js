b99e344f1b63739565820c5e6440eb406d0f196f
ekorn <ekorn@sofaecke.org>
2011-09-01 10:54:13 +0200

    add the last dependency

--- viewer/app.js
+++ viewer/app.js
@@ -18,12 +18,28 @@ ddoc.views = {};
 
 ddoc.views.listArchives = {
   map: function(doc) {
-    if(doc.archive_info != null) {
+    if(doc.archive_info !== null) {
       emit(doc.archive_info.keyword, {"id": doc._id,"keyword" : doc.archive_info.keyword, "description": doc.archive_info.description,"count": doc.archive_info.count});
     }
   }
 }
 
+ddoc.views.getGeo = {
+  map: function(doc) {
+    if(doc.archive_info != null) {
+      emit(doc._id, doc.geoMarker);
+    }
+  }
+}
+
+ddoc.views.getGeo2 = {
+  map: function(doc) {
+    for(var i=0; i<doc.geoMarker.length; i++){
+      emit(doc.geoMarker[i].lat, doc.geoMarker[i]);
+    }
+  }
+}
+//http://twapperlyzer.cs.upb.de:5984/twapperlyzer/_design/twapperlyzer/_view/listArchives?key="%23cscw11"
 
 
 ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {   
